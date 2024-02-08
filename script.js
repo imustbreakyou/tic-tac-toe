afcdf3e393edeb2b8aa956826a6cf7e5fab4cb45
Mike <mw@Mikes-MacBook-Pro.local>
2024-02-08 12:22:28 -0800

    implement win condition
    TODO: fix continuation text input

--- script.js
+++ script.js
@@ -12,7 +12,13 @@ function gameController() {
 
     let player1 = createPlayer('Rocky', 'X');
     let player2 = createPlayer('Drago', 'O');
+    console.log(`${player1.name} vs ${player2.name}!`);
+    
+  
+    
     let game;
+
+
 
     function startGame() {
         game = {
@@ -40,11 +46,8 @@ function gameController() {
         displayBoard(game);
         let cell = getUserInput(player);
         placeMarks(cell, player);
-        checkGameStatus();
-        if (!game.isOver) {
-            switchPlayers();
-            playRound();
-        }
+        checkGameStatus(game, player);
+       
     }
 
     function displayBoard(game) {
@@ -82,27 +85,33 @@ function gameController() {
     function checkWin(game) {
         // check rows
         const size = 3;
+
         for (let i = 0; i < size; i++) {
             if (game.board[i][0] === game.board[i][1] && game.board[i][0] === game.board[i][2]  && game.board[i][0] !== ' ') {
                 game.isOver = true;
+                console.log(`game over! win on ${i} row!`);
                 
             }
         }
 
         // check columns
      
-        const size = 3;
+        
         for (let i = 0; i < size; i++) {
             if (game.board[0][i] === game.board[1][i] && game.board[0][i] === game.board[2][i] && game.board[0][i] !== ' ') {
                 game.isOver = true;
+                console.log(`game over! win on ${i} column!`);
             }
         }
    
 
-        // check diagnals
+        // check diagonals
+        // downwards
         if ((game.board[1][1] === game.board[0][0] && game.board[1][1] === game.board[2][2] && game.board[1][1] !== ' ') ||
-            (game.board[1][1] === game.board[0][2] && game.board[1][1] === game.board[2][2] && game.board[2][0] !== ' ')) {
+        // upwards
+            (game.board[1][1] === game.board[2][0] && game.board[1][1] === game.board[0][2] && game.board[1][1] !== ' ')) {
                 game.isOver = true;
+                console.log(`game over! diagonal win!`);
             }
         
 
@@ -110,13 +119,30 @@ function gameController() {
            
     }
 
-    function checkGameStatus() {
-        checkWin(); // Check win conditions
+    function checkGameStatus(game, player) {
+        checkWin(game); // Check win conditions
         // Update game status based on win or tie
         if (game.isOver) {
             console.log(`Game over! ${player.name} (${player.mark}) wins! \n`);
-        }
+            continueGameQuery();
+            
+        } else {
+            switchPlayers();
+            playRound();
+        };
     }
+
+    function continueGameQuery() {
+
+        let continuePrompt = readlineSync.questionInt(`continue game? (y/n) \n`);
+        if (continuePrompt ==='y' || 'Y') {
+            startGame();
+        } else {
+            console.log('Thanks for playing!');
+            return;
+        };
+    }
+
 
     function switchPlayers() {
         player1.isActive = !player1.isActive;
